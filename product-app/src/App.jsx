9811b900043f32bbf40fa21651420cd9639940df
Niranjan-3901 <niranjankumarbarai456@gmail.com>
2024-07-13 10:31:55 +0530

    React Version-19 error control

--- product-app/src/App.jsx
+++ product-app/src/App.jsx
@@ -1,3 +1,8 @@
+
+// if React-19 is not working change it's versions to given version in package.json file
+// "react": "^18.3.1",
+// "react-dom": "^18.3.1",
+
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductListNew from './components/productListNew';
